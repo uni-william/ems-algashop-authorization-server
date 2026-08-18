e0c6bbd6f26a8ac73f1b1ab58c8e4bdd3ed1b441
William Gomes <william.desenv@gmail.com>
2026-08-18 10:24:09 -0400

    Update

---
+++ src/main/java/com/algaworks/algashop/authorizationserver/infrastructure/discovery/EurekaClientConfig.java
@@ -0,0 +1,9 @@
+package com.algaworks.algashop.authorizationserver.infrastructure.discovery;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@Configuration
+@EnableDiscoveryClient
+public class EurekaClientConfig {
+}
